b5296d671719379d970b50cf93f639f87921c7f4
czulueta <https://github.com/czulueta/weed.git>
2020-12-02 00:11:27 -0700

    setup basic client and basic server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("hello Cesar")
+})
+
+app.listen(9000, () => {
+    console.log("server is running on port 9000!!!")
+})
